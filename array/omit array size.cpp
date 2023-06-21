fc86a8c19c2dea5bd26dac9f2b36b4677bb6dbc9
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-21 20:00:29 +0530

    Create omit array size.cpp

---
+++ array/omit array size.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int a[] = {1,2,3,4,5,6,7,8,9};
+    cout << "We have initiated an array with no specified number of elements." <<endl;
+
+    
+}
+
+// this way we can make arrays in c++ without specifying the size of the array for example int a[9]; the compiler is smart enough to understand the size of the array and number of elements to be inserted without us spefcifying it's size.
