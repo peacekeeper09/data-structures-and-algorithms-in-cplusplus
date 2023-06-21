c4f3f3f7862ed680004532f785f387932f21dba7
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-21 12:40:27 +0530

    Update array-basic.cpp

--- array/array-basic.cpp
+++ array/array-basic.cpp
@@ -1,1 +1,11 @@
-.
+// declaring an array and accessing all the values in it.
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a[4] = {1,2,3,4};
+    int i;
+    for (i =0; i <4; i++) {
+        cout << a[i] << endl;
+    }
+}
