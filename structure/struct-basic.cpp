16e7db745b41658cd4ba5cb119ec81277ff9f031
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-22 09:01:09 +0530

    Update struct-basic.cpp

--- structure/struct-basic.cpp
+++ structure/struct-basic.cpp
@@ -5,17 +5,20 @@ struct {
     int length;
     int breadth;
 }rect1;
-// rect1 
+
 struct rectangle {
     int l;
     int b;
 }rect2;
-//rect2
+
 int main() {
     rect1.length = 10;
     rect1.length = 15;
 
     rect2.l = 15;
     rect2.b = 20;
+
+    rectangle rect3 = {12,3};
+    struct rectangle rect4 = {40,25};
 
 }
