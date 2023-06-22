c1b64868922d937b47d48a7570a3098b1b63b763
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-22 20:03:44 +0530

    Create memory address.cpp

---
+++ structure/memory address.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    string food = "Tofu";
+    cout << "Value stored in variable food is " << food << " and it's memory address is " << &food <<endl;
+}
+
+// &variable tells you the memory address of the variable
