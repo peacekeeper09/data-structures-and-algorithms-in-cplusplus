a80b18025ad271a08dda13b19a8dcf6a148952be
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-22 08:54:10 +0530

    Create struct-basic.cpp

---
+++ structure/struct-basic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct {
+    int length;
+    int breadth;
+}rect1;
+
+struct rectangle {
+    int l;
+    int b;
+}rect2;
+
+int main() {
+    rect1.length = 10;
+    rect1.length = 15;
+
+    rect2.l = 15;
+    rect2.b = 20;
+
+}
